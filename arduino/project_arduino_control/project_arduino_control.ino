109396f3c59f20ffbcd8582bce952ca76a0a3006
Lev Stambler <levstamb@gmail.com>
2023-12-08 15:32:58 -0500

    progress

--- arduino/project_arduino_control/project_arduino_control.ino
+++ arduino/project_arduino_control/project_arduino_control.ino
@@ -4,14 +4,22 @@
 #define STEP_PIN 18 // Step pin
 #define STEPS_PER_REV 200 // Change this to fit the number of steps per revolution for your motor
 
-#define PIN_NEO_PIXEL  5  // pin 11 connected to NeoPixel
+#define PIN_NEO_PIXEL  4  // pin 11 connected to NeoPixel
 #define NUM_PIXELS     100  // The number of LEDs (pixels) on NeoPixel
+
+#define DEBUG true
 
 Adafruit_NeoPixel strip(NUM_PIXELS, PIN_NEO_PIXEL, NEO_GRB + NEO_KHZ800);
 
-long ang = 0;
-int delaytime = 1000;
+//long ang = 0;
+//int delaytime = 1000;
 
+long ang = 0;
+long currStepForAng = 0;
+int delaytime = 0;
+int pixelsR[NUM_PIXELS];
+int pixelsG[NUM_PIXELS];
+int pixelsB[NUM_PIXELS];
 
 void blue_green_gradient(int all_on=0) {
   strip.clear();
@@ -24,7 +32,6 @@ void blue_green_gradient(int all_on=0) {
     delay(50); 
   }
   for (int i = 0; i < NUM_PIXELS; i++) {
-    
     strip.setPixelColor(NUM_PIXELS - i - 1, strip.Color(0, 0, 0));
     strip.show();
     delay(50); 
@@ -32,21 +39,38 @@ void blue_green_gradient(int all_on=0) {
 }
 
 void set_strip(int pixel, int R, int G, int B) {
-  strip.setPixelColor(pixel, strip.Color(R, G, B));
+//  strip.clear();
+  pixelsR[pixel] = R;
+  pixelsG[pixel] = G;
+  pixelsB[pixel] = B;
+//  strip.setPixelColor(pixel, strip.Color(R, G, B));
+  for (int i = 0; i < NUM_PIXELS; i++) {
+//   int i = pixel;
+//    strip.setPixelColor(pixel, pixelsR[i], pixelsG[i], pixelsB[i]);
+    strip.setPixelColor(i, pixelsR[i], pixelsG[i], pixelsB[i]);
+  }
   strip.show();
 }
 
 void setup() {
+  for (int i = 0; i < NUM_PIXELS; i++) {
+    pixelsR[i] = 0;
+    pixelsG[i] = 0;
+    pixelsB[i] = 0;
+  }
   pinMode(DIR_PIN, OUTPUT);
   pinMode(STEP_PIN, OUTPUT);
   Serial.begin(115200);
   strip.begin();
+  strip.show();
+  if (DEBUG)
+    blue_green_gradient(0);
 }
 
 void loop() {
 //  TODO: remove
-  while (true)
-    blue_green_gradient(0);
+//  while (true)
+//    
   if (Serial.available() > 0) {
     String command = Serial.readStringUntil('\n'); // Read the command from serial
     //    L:<Pixel Number>:R:G:B     -- 3 per digit
@@ -55,12 +79,18 @@ void loop() {
       int R = command.substring(6, 9).toInt();
       int G = command.substring(10, 13).toInt();
       int B = command.substring(14, 17).toInt();
+      if (DEBUG)
+        Serial.printf("Setting pixel %d to (%d, %d, %d)\n", pixel, R, G, B);
       set_strip(pixel, R, G, B);
-    } else {
+    } else if ((char) command[0] == 'C') {
+      strip.clear();
+    }else {
       readSerial(command, &ang, &delaytime);
-      rotate(ang, delaytime);
+      currStepForAng = 0;
+      rotate();
     }
   }
+  /************ Perform the looping *********/
 }
 
 // +:000360:01000
@@ -84,13 +114,32 @@ void readSerial(String command, long * ang, int * delaytime) {
   }
 }
 
-void rotate(long ang, int delaytime) {
+void rotate() {
   int dir = ang < 0;
   digitalWrite(DIR_PIN, dir); // Set the direction
 
   int ang_pos = ang < 0 ? ang * -1 : ang;
+  currStepForAng += ang < 0 ? -1 : 1;
 
   long n_steps = (ang_pos * STEPS_PER_REV) / 360;
+
+  //  Perform a step
+//  if ((ang < 0 && n_steps <= currStepForAng) || (ang > 0 && currStepForAng <= n_steps)) {
+//    digitalWrite(STEP_PIN, HIGH);
+//    delayMicroseconds(delaytime);
+//    digitalWrite(STEP_PIN, LOW);
+//    delayMicroseconds(delaytime);
+//  }
+
+
+//  if (dir < 0 && ang < currAng) {
+//    digitalWrite(STEP_PIN, HIGH);
+//    delayMicroseconds(delaytime);
+//    digitalWrite(STEP_PIN, LOW);
+//    delayMicroseconds(delaytime);
+//  }
+//  
+//
   for (long i = 0; i < n_steps; i++) {
     digitalWrite(STEP_PIN, HIGH);
     delayMicroseconds(delaytime);
--- beat.py
+++ beat.py
@@ -1,4 +1,6 @@
+import time
 import librosa
+import math
 import threading
 import librosa.display
 import numpy as np
@@ -6,13 +8,32 @@ import sounddevice as sd
 import soundfile as sf
 import serial
 
-ser = serial.Serial('/dev/ttyUSB0', 115200)
+N_PIXELS = 100
+DEBUG = True
 
+ser = serial.Serial('/dev/ttyUSB0', 115200)
+# serLights = serial.Serial('/dev/ttyUSB1', 115200)
+serLights  = ser
+
+i = 0
 
 def sendSerData(data):
     data += "\r\n"
-    print("SENDING DATA", data)
+    if DEBUG:
+        global i
+        i += 1
+        print("SENDING DATA", i, data)
     ser.write(data.encode())
+
+def sendSerLights(data):
+    data += "\r\n"
+    if DEBUG:
+        global i
+        i += 1
+        print("SENDING DATA", i, data)
+    serLights.write(data.encode())
+
+
 
 def formatAngleMove(angle, delayMicroSec=500):
     dir = "+" if angle > 0 else "-"
@@ -21,10 +42,39 @@ def formatAngleMove(angle, delayMicroSec=500):
     delayMicroSec = str(delayMicroSec).zfill(5)[:5]
     return f"{dir}:{angle}:{delayMicroSec}"
 
+def formatPixelSet(pixel, R, G, B):
+    return f"L:{str(pixel).zfill(3)}:{str(R).zfill(3)}:{str(G).zfill(3)}:{str(B).zfill(3)}"
+
 
 def save_wav_file(y, sr, file_path):
     sf.write(file_path, y, sr)
 
+def generateGaussianRandomDownwardLight(time_steps: int, volumes: list[int], gaussian_means: list[list[float]], gaussian_stds: list[list[float]],
+                                        n_lights=N_PIXELS, top_pixel_is_max = True):
+    """
+        Sample from a Gaussian distribution which should have width to around 255ish.
+        We will re-sample until we get a value that is within the range of 0-255.
+    """
+    assert len(gaussian_means) == len(gaussian_stds), "Must have same number of means and stds"
+    def sample():
+        idx = np.random.choice(len(gaussian_means))
+        normal_samp = np.random.normal(gaussian_means[idx], gaussian_stds[idx])
+        if normal_samp.max() > 255 or normal_samp.min() < 0:
+            return sample()
+        return normal_samp.round().astype(int)
+    cmds = []
+
+    if not top_pixel_is_max:
+        raise NotImplementedError("Only top pixel is max is implemented")
+    for i in range(time_steps):
+        # TODO:: idk
+        pixel = (n_lights - i % n_lights) - 1
+        # if pixel 
+        cmd = formatPixelSet(pixel, *sample())
+        print("Color command", cmd)
+        cmds.append(cmd)
+    return cmds
+    # Start
 
 def amplify_on_downbeats(y, sr, downbeat_times, window=0.1, amplification_factor=1.5):
     # Calculate the number of samples to mute based on mute_duration
@@ -38,8 +88,7 @@ def amplify_on_downbeats(y, sr, downbeat_times, window=0.1, amplification_factor
         y[start_sample:end_sample] *= amplification_factor
     return y
 
-
-def get_downbeat_sample_moments(y, sr):
+def get_sample_moments(y, sr, downbeat_subdivisions=16):
     # Calculate the onset envelope
     onset_env = librosa.onset.onset_strength(y=y, sr=sr)
 
@@ -55,58 +104,84 @@ def get_downbeat_sample_moments(y, sr):
     # measures = int(tempo) # Initial assumption
 
     print(len(beats))
-    new_tempo = 0
     for i in range(1, len(beats)):
         # Dynamically adjust the expected measure interval based on adjacent beat intervals
-        if (i - 1) % 2 == 0:
+        INTERV = 1
+        if (i - 1) % INTERV == 0:
             downbeats.append(beats[i])
-        if i >= 2:
-            interv = (beats[i] - beats[i-2]) / (2 * sr)
-            # print(interv)
-            new_tempo = 60 / (interv)
-        # print(new_tempo)
+        
+    def add_downbeat_time_steps(mul_fact=16):
+        volumes = []
+        steps = []
+        for i, _ in enumerate(downbeats):
+            if i >= 1:
+                period = downbeats[i] - downbeats[i - 1]
+                for j in range(mul_fact):
+                    steps.append(int(round(downbeats[i - 1] + (period / mul_fact) * j)))
+                    start_index = max(beats[i] - 10, 0)  # Ensuring the start index is not negative
+                    end_index = min(beats[i] + 10, len(y))  # Ensuring the end index does not exceed the length of y
+                    average_volume = np.mean(np.abs(y[start_index:end_index]))
+                    volumes.append(average_volume)
+        return steps, volumes
 
-        # if measures != 0 and (i % measures) == 0:
-        #     print("ADDING DOWNBEAT", len(downbeats))
-        #     downbeats.append(beats[i])
-
-        # TODO: wjat these frames?
-    # downbeat_times = librosa.frames_to_time(downbeats, sr=sr)
-    return downbeats
+    sample_timestamps, volumes = add_downbeat_time_steps()
+    return sample_timestamps, volumes
 
 
 def send_serial_commands_at_downbeats(downbeat_times, sr):
-    for downbeat_time in downbeat_times:
+    for i, downbeat_time in enumerate(downbeat_times):
         # Calculate the time to wait until the next downbeat
         wait_time = downbeat_time / sr
+        direction = 1 if i % 2 == 0 else -1
         threading.Timer(wait_time, sendSerData, args=[
-                        formatAngleMove(100)]).start()
+                        formatAngleMove(direction * 100)]).start()
 
+def send_light_commands_at_downbeats(downbeat_times, sr, cmds):
+    for i, downbeat_time in enumerate(downbeat_times):
+        # print(volumes[i], i)
+        # Calculate the time to wait until the next downbeat
+        wait_time = downbeat_time / sr
+        threading.Timer(wait_time, sendSerData, args=[cmds[i]]).start()
 
-def do_it(filename, window_length=0.2, amplification_factor=1.5):
+def clearLights():
+    sendSerData("C")
+
+def do_it(filename, window_length=0.2, amplification_factor=1.2):
     # Load the audio file
     y, sr = librosa.load(filename)
-    downbeat_times = get_downbeat_sample_moments(y, sr)
-    y_amplified = amplify_on_downbeats(
-        y, sr, downbeat_times, amplification_factor=amplification_factor, window=window_length)
+    time_steps, volumes = get_sample_moments(y, sr)
 
+    # y_amplified = amplify_on_downbeats(
+    #     y, sr, downbeat_times, amplification_factor=amplification_factor, window=window_length)
+
+    clearLights()
+    time.sleep(1)
     # Save the modified file
-    save_wav_file(y_amplified, sr, "out.wav")
+    # save_wav_file(y, sr, "out.wav")
+
+    lightCmds = generateGaussianRandomDownwardLight(len(time_steps), volumes, [[150, 10, 200], [20, 150, 200], [255, 0, 0], [10, 10, 10]], [[20, 2, 50], [10, 40, 50], [50, 20, 20], [2, 2, 2]])
+    print("LEN LIGHTS", len(time_steps), len(lightCmds))
 
     # Create a thread for playing the music
-    play_thread = threading.Thread(target=sd.play, args=(y_amplified, sr))
+    play_thread = threading.Thread(target=sd.play, args=(y, sr))
 
     # Start the play thread
-    play_thread.start()
 
     # Send serial commands at downbeats in a separate thread
-    send_serial_commands_thread = threading.Thread(
-        target=send_serial_commands_at_downbeats, args=(downbeat_times, sr))
-    send_serial_commands_thread.start()
+    # send_motor_commands_thread = threading.Thread(
+    #     target=send_serial_commands_at_downbeats, args=(time_steps, sr))
 
+    send_light_commands_thread = threading.Thread(
+        target=send_light_commands_at_downbeats, args=(time_steps, volumes, sr, lightCmds))
+
+    play_thread.start()
+    # send_motor_commands_thread.start()
+    send_light_commands_thread.start()
+    
     # Wait for the play thread to finish
     play_thread.join()
-    send_serial_commands_thread.join()
+    # send_motor_commands_thread.join()
+    send_light_commands_thread.join()
 
 # def do_it(filename, window_length=0.2, amplification_factor=.1):
 #     # Load the audio file
@@ -121,7 +196,7 @@ def do_it(filename, window_length=0.2, amplification_factor=1.5):
 
 if __name__ == "__main__":
     # Example:
-    filename = 'EtudeC.mp3'
+    filename = 'GirlFellTrimmed.mp3'
     # filename = 'raindrop.mp3'
     upbeats = do_it(filename)
     print(upbeats)
